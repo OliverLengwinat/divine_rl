0cf025111f4b2de8e12966d4cf8ee624cc71a6a1
Patrick <patrickhart.1990@gmail.com>
2018-09-04 16:23:20 +0200

    split structred and unstructured

---
+++ src/divine/run_unstructured.py
@@ -0,0 +1,28 @@
+import numpy as np
+import random
+from src.environment.environment import Environment
+
+if __name__ == '__main__':
+
+	# load environment
+	env = Environment("tests/python/unstructured_world.pb.txt")
+	env.debug_world_plot()
+
+	# randomly chosen system input
+	u = np.array([[random.uniform(-0.5, 0.5), random.uniform(-0.5, 0.5)]])
+
+	# run until complete
+	running = True
+	while running:
+		for agent in env.agents:
+			state, action, next_state, reward, is_final = agent.step(u, 0.25)
+			if is_final:
+				print(state, action, next_state, reward)
+				running = False
+				env.reset()
+		
+		# plot agents
+		env.debug_agents_plot()
+
+	# show plots
+	env.debug_plot_show()
