690aeb671d794579ea29a6c63affd1659103ea49
Patrick <patrickhart.1990@gmail.com>
2018-09-13 21:10:03 +0200

    .

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    sudo apt-get install python3 python3-dev python3-pip virtualenv openjdk-8-jdk curl npm -y
-    echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-    curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
-    sudo apt-get update -y && sudo apt-get install bazel -y
-    sudo apt-get upgrade bazel
-    sudo apt-get update && apt-get install -y python3-pip python3-dev python3-tk 
+    apt-get install python3 python3-dev python3-pip virtualenv openjdk-8-jdk curl npm -y
+    echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
+    curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+    apt-get update -y && apt-get install bazel -y
+    apt-get upgrade bazel
+    apt-get update && apt-get install -y python3-pip python3-dev python3-tk 
 fi
 
 virtualenv -p python3 venv
